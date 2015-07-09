9c591066cf4d20105dd866534e4daa555945d968
jin <lyriccjin@foxmail.com>
2015-07-10 01:24:24 +0800

    已经调试通过前后端通信以及数据绑定 接下来把数据库插入搞定就OK了 然后就是用户帐号密码登入的功能

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 "use strict"
 
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+
+app.post('/', function(request, response){
+	console.log("post. " + JSON.stringify(request.body));
+	response.send("nihao");
+});
 
 app.listen(8080);
